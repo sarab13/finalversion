c495c261fa3073047fbc9c990b6c34a976104ffe
sarab13 <sarabaliofficial@gmail.com>
2021-11-19 21:09:59 -0800

    updated

--- app.js
+++ app.js
@@ -1,39 +1,66 @@
-var flag=true
+var flag=false
 var offset=0
-var count=1
 var condition=0
-const con=document.getElementById('container')
-//con.setAttribute('onscroll',checkhi)
+const container=document.getElementById('container')
 
+const sortByDate = arr => {
+  const sorter = (a, b) => {
+     return new Date(b.import_datetime).getTime() - new Date(a.import_datetime).getTime();
+  }
+  arr.sort(sorter);
+};
 
-//console.log(rect)
-//while(flag!=false){
 function apiCalling(){
 fetch(`https://api.giphy.com/v1/gifs/search?q=cats&api_key=tv656ue2Z5w8ENrWU9YVpfNUkmdIMFP2&limit=40&offset=${offset}`)
   .then(response => response.json())
   .then(async json => {
-    //var con=document.getElementById('container')
-    await json.data
-      .map(gif => gif.images.original.url)
-      .forEach(url => {
+    const data=json.data
+    if(flag)
+    sortByDate(data)
+    data.map(gif => {
         let img = document.createElement('img')
-        img.src = url;
-        con.appendChild(img)
-      })
+        let a=document.createElement('a')
+        a.href=gif.url
+        img.src = gif.images.original.url;
+        a.appendChild(img)
+        container.appendChild(a)})
+
   })
   .catch(error => con.appendChild = error)
   offset+=40
   condition-=800;
 
 }
-function checkhi(){
-  const rect=con.getBoundingClientRect();
-  console.log(condition)
+function fetchMore(){
+  const rect=container.getBoundingClientRect();
   if(rect.top<condition)
   {
     apiCalling()
   }
-  console.log(rect)
+}
+function doSorting(btn){
+  const relevant=document.getElementById('relevant')
+  const newest=document.getElementById('newest')
+  if(btn=="relevant"){
+    offset=0;
+    flag=false;
+    relevant.style.color="white";
+    relevant.style.backgroundColor="black";
+    newest.style.color="black";
+    newest.style.backgroundColor="white"
+    container.querySelectorAll('*').forEach(n=>n.remove())
+    apiCalling()
+  }
+  else{
+    offset=0
+    flag=true
+    newest.style.color="white"
+    newest.style.backgroundColor="black"
+    relevant.style.color="black"
+    relevant.style.backgroundColor="white"
+    container.querySelectorAll('*').forEach(n=>n.remove())
+    apiCalling()
+  }
 }
 
 apiCalling()
